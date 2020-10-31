e620c9142ca864ed0ff9ff85cab27f605520f5ca
jatinvyas1 <jatin.vyas@outlook.com>
2020-10-31 17:50:58 +0530

    initial

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,6 @@ function App()
 	<div id="main">
 	//Do not alter main div
 	//Please do not alter the functional component as tests depend on the type of component.
-	<textarea id = "task"></textarea>
-	<button id = "btn" onClick={handleClick}></button>
 	</div>
 	);
 }
