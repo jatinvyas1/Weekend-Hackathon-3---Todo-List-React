91dad8980fb11419cb763c1569250763930b02dc
jatinvyas1 <jatin.vyas@outlook.com>
2020-10-31 18:41:53 +0530

    commit

--- src/components/toDoList.jsx
+++ src/components/toDoList.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
-function TodoList (){
-    return (
-        <>
-        <textarea id="task"></textarea>
-        <button id="btn"></button>
-        </>
-    )
+function TodoList() {
+  let [tasks, setTasks] = useState([]);
+  const handleClick = () => {
+    const taskArea = document.getElementById("task");
+    if (taskArea.value === "") {
+      return;
+    }
+    setTasks([...tasks, taskArea.value]);
+  };
+  const handleEdit = (index) => {
+    console.log(index);
+  };
+  const handleDelete = (index) => {
+    console.log(index);
+  };
+
+  return (
+    <>
+      <textarea id="task"></textarea>
+      <button id="btn" onClick={handleClick}></button>
+      {tasks.map((element, index) => {
+        return (
+          <div className="list" key={index}>
+            {element}
+            <button className="edit" onClick={() => handleEdit(index)}>
+              E
+            </button>
+            <button className="delete" onClick={() => handleDelete(index)}>
+              D
+            </button>
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default TodoList;
