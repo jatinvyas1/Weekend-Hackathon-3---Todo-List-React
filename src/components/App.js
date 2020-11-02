3a5f7f4f16e2a99d841328560c5bc13bab659c96
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-02 22:14:23 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import "./../styles/App.css";
 
+function Task({ currentIndex, handleEdit, handleDelete, element }) {
+  const [shouldEdit, setShouldEdit] = useState(false);
+  const [currentTask, setCurrentTask] = useState("");
+
+  const currentTaskChange = (event) => {
+    setCurrentTask(event.target.value);
+  };
+
+  const handleSave = () => {
+    if (currentTask.length === 0) {
+      return;
+    }
+    handleEdit(currentIndex, currentTask);
+    setCurrentTask("");
+    setShouldEdit(false);
+  };
+
+  const editTask = () => setShouldEdit(true);
+  const deleteTask = () => handleDelete(currentIndex);
+
+  return (
+    <div className="list">
+      {element}
+      <button className="edit" onClick={editTask}>
+        E
+      </button>
+      <button className="delete" onClick={deleteTask}>
+        D
+      </button>
+      {shouldEdit ? (
+        <>
+          <textarea
+            id="task"
+            onChange={currentTaskChange}
+            value={currentTask}
+          ></textarea>
+          <button id="btn" onClick={handleSave}>
+            +
+          </button>
+        </>
+      ) : null}
+    </div>
+  );
+}
+
 function App() {
+  let [tasks, setTasks] = useState([]);
+  let [newTask, setNewTask] = useState("");
+  const currentTaskChange = (event) => {
+    setNewTask(event.target.value);
+  };
+  const handleClick = () => {
+    if (newTask === "") {
+      return;
+    }
+    setTasks([...tasks, newTask]);
+    setNewTask("");
+  };
+  const handleEdit = (indexToChange, newTask) => {
+    const temp = [...tasks];
+    temp[indexToChange] = newTask;
+    setTasks(temp);
+  };
+  const handleDelete = (indexToDelete) => {
+    const temp = tasks.filter((element, index) => index !== indexToDelete);
+    setTasks(temp);
+  };
+
   return (
     <div id="main">
-      {/* // //Do not alter main div
-	// //Please do not alter the functional component as tests depend on the type of component. */}
+      <textarea
+        id="task"
+        onChange={currentTaskChange}
+        value={newTask}
+      ></textarea>
+      <button id="btn" onClick={handleClick}>
+        +
+      </button>
+      {tasks.map((element, index) => {
+        return (
+          <Task
+            key={index}
+            currentIndex={index}
+            element={element}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+          />
+        );
+      })}
     </div>
   );
 }
