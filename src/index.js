2d780de2b03077797118be9a00d6e8e418bbf1bb
jatinvyas1 <48468154+jatinvyas1@users.noreply.github.com>
2020-11-03 21:15:08 +0530

    finalC

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import TodoList from "./components/toDoList";
 
-ReactDOM.render(
-<App />
-, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
